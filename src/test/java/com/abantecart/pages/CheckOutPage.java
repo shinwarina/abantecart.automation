17ecd894bedeeeb0306f67d04cfc253508e4aedf
denis <denis@192.168.13.1>
2018-04-03 14:41:58 -0400

    CheckOutPage

--- src/test/java/com/abantecart/pages/CheckOutPage.java
+++ src/test/java/com/abantecart/pages/CheckOutPage.java
@@ -1,5 +1,35 @@
 package com.abantecart.pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.abantecart.utilities.Driver;
+
 public class CheckOutPage {
 
+	private WebDriver driver;
+
+	public CheckOutPage(WebDriver driver) {
+		this.driver=driver;
+		PageFactory.initElements(driver, this);
+	}
+
+	
+	public WebElement checkout_btn;
+	
+	@FindBy(linkText ="Edit Shipping")
+	public WebElement editShipping;
+	//table[@class='table confirm_shippment_options']/tbody/tr/td//a
+	
+	@FindBy(linkText ="Edit Payment")
+	public WebElement editPayment;
+
+	@FindBy(linkText ="Edit Coupon")
+	public WebElement editCoupon;
+	
+	@FindBy(linkText ="Edit Cart")
+	public WebElement editCart;
+	
 }
