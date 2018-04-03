7bf5aef317d6b60388c41ad0f2d7ded48c4c06de
BusaraMamat <busara@192.168.13.11>
2018-04-03 14:33:35 -0400

    Account Page and Cart page

--- src/test/java/com/abantecart/pages/AccountPage.java
+++ src/test/java/com/abantecart/pages/AccountPage.java
@@ -1,5 +1,49 @@
 package com.abantecart.pages;
 
-public class AccountPage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
+public class AccountPage {
+	private WebDriver driver;
+
+	public AccountPage(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+	}
+	
+	public static boolean verifyAccountPageIsLoaded(WebDriver driver) {
+		String urlExpected = "http://34.223.219.142:1022/abantecart/index.php?rt=account/account";
+		if(driver.getCurrentUrl().equals(urlExpected) ) {
+			return true;
+		}
+		return false;
+	}
+	
+	@FindBy(linkText = "Account Dashboard")
+	public WebElement AccountDashboard;
+	
+	@FindBy(linkText = " My wish list")
+	public WebElement Mywishlist;
+	
+	@FindBy(linkText = " Edit account details")
+	public WebElement  Editaccountdetails;
+	
+	
+	@FindBy(linkText = "Change password")
+	public WebElement Changepassword;
+	
+	  
+	@FindBy(linkText = "Manage Address Book")
+	public WebElement ManageAddressBook;
+	
+	@FindBy(linkText = "  Order history")
+	public WebElement   Orderhistory;
+	
+	 
 }
+
+
+
+
--- src/test/java/com/abantecart/pages/CartPage.java
+++ src/test/java/com/abantecart/pages/CartPage.java
@@ -1,5 +1,30 @@
 package com.abantecart.pages;
 
-public class CartPage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
+public class CartPage {
+	private WebDriver driver;
+
+	public CartPage(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+	}
+	
+	
+	public static boolean verifyAccountPageIsLoaded(WebDriver driver) {
+		String urlExpected = "http://34.223.219.142:1022/abantecart/index.php?rt=checkout/cart";
+		if(driver.getCurrentUrl().equals(urlExpected) ) {
+			return true;
+		}
+		return false;
+	}
+	
+	@FindBy(id="cart_checkout1")
+	public WebElement Checkout;
+	
+	@FindBy(id="cart_update")
+	public WebElement Update;
 }
