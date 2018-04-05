e546b95092197126c916407148abed26fe09842f
artempashynskyi <mailofartem@yahoo.com>
2018-04-05 15:13:32 -0400

    fix

--- src/test/java/com/abantecart/step_defs/BuyingItemStepDefinitions.java
+++ src/test/java/com/abantecart/step_defs/BuyingItemStepDefinitions.java
@@ -13,7 +13,7 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
-public class Buying_item_Step_defs {
+public class BuyingItemStepDefinitions {
 	DashboardPage dashBoardPage = new DashboardPage();
 	SearchResultPage searchResultPage = new SearchResultPage();
 	HomePage homePage = new HomePage();
