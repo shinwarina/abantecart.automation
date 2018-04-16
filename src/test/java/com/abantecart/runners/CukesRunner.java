8bd8f2804b4beff2c4a022f081e86b13c00383a4
artempashynskyi <mailofartem@yahoo.com>
2018-04-16 15:02:02 -0400

    Runner change

--- src/test/java/com/abantecart/runners/CukesRunner.java
+++ src/test/java/com/abantecart/runners/CukesRunner.java
@@ -4,7 +4,10 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 
 @CucumberOptions(
-		plugin= {"pretty","html:target/cucumber-report"},
+		plugin = {"pretty", 
+				"html:target/cucumber-report",
+				"json:target/cucumber.json"
+		},
 		tags= "@Smoke",
 		features="src/test/resources/com/abantecart/features",
 		glue="com/abantecart/step_defs",
